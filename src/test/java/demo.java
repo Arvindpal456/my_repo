557cb53596076409e3c04189867ff0ca3e12322f
arvindpals444@gmail.com <arvindpalsinghkarwal@Arvindpals-MacBook-Air.local>
2024-07-06 15:52:08 +0530

    script change

--- src/test/java/demo.java
+++ src/test/java/demo.java
@@ -3,7 +3,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("3rd Version");
         System.out.println("not add 2");
-        System.out.println("not add 2");
+        System.out.println("not add 3");
 
     }
 }
